78d79da62ec806556280d88d6296d3a8fe3f37c5
Толмачев Никита Александрович <ntolmachev@fil-it.ru>
2026-02-07 00:23:44 +0300

    init commit

---
+++ src/main/java/ru/tolmachev/clientcollector/ClientcollectorApplication.java
@@ -0,0 +1,13 @@
+package ru.tolmachev.clientcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientcollectorApplication.class, args);
+	}
+
+}
